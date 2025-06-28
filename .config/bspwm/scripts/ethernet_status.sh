30066cec9e78d18685cb933f52fdc6fc2a9cc07a
sammy-ulfh <sammy_ulfh@proton.me>
2025-06-28 13:11:48 +0000

    Fix errors

--- .config/bspwm/scripts/ethernet_status.sh
+++ .config/bspwm/scripts/ethernet_status.sh
@@ -3,11 +3,11 @@
 interface=$(ip route | grep -i "default" | grep -v "tun0" | awk '{print $5}' | sort -u)
 num_interfaces=$(echo "$interface" | wc -l)
 
-if [ "$(ifconfig | grep -i "$interface" -A 1 | grep -i 'inet')" ] && [ "$interface" ]; then
+if [ "$(/sbin/ifconfig | grep -i "$interface" -A 1 | grep -i 'inet')" ] && [ "$interface" ]; then
   if [ "$num_interfaces" -gt 1 ]; then
     echo "%{F#00FF00}󱚸 %{F#ffffff} Multiple interfaces%{u-}"
   else
-    echo "%{F#00FF00}󱚸 %{F#ffffff}$(ifconfig | grep -i "$interface" -A 1 | grep -iw 'inet' | awk '{print $2}')%{u-}"
+    echo "%{F#00FF00}󱚸 %{F#ffffff}$(/sbin/ifconfig | grep -i "$interface" -A 1 | grep -iw 'inet' | awk '{print $2}')%{u-}"
   fi  
 else
   echo "%{F#FF0000}󰤭 %{F#ffffff} NO WIFI%{u-}"
